a7af8d115e90310bd7bf254a5328aa52f6d3ede8
David Horvát <horvatda@proton.me>
2024-03-19 19:31:07 +0100

    created background

--- src/ui.rs
+++ src/ui.rs
@@ -4,13 +4,25 @@ use ratatui::{
     widgets::{Block, BorderType, Paragraph, Gauge},
     Frame,
 };
-use ratatui::layout::{Constraint, Direction};
+use ratatui::layout::{Constraint, Direction, Rect};
 use ratatui::widgets::Borders;
 
 use crate::app::App;
 
+/// renders application
 pub fn render(app: &mut App, frame: &mut Frame) {
+    let area: Rect = set_background(frame);
+}
 
+/// set background
+fn set_background(frame: &mut Frame) -> Rect {
+    let area = frame.size();
+    let background = Block::default()
+        .borders(Borders::NONE)
+        .style(Style::new().bg(Color::Rgb(32, 32, 36)));
+
+    frame.render_widget(&background, area);
+    background.inner(area)
 }
 
 
