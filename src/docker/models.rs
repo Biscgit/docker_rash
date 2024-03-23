4528ae55f633f5813b384c3769c6005485edd778
David Horvát <horvatda@proton.me>
2024-03-23 20:27:29 +0100

    created a base working json model

---
+++ src/docker/models.rs
@@ -0,0 +1,36 @@
+// use std::collections::HashMap;
+// use serde_json::Value;
+use serde::{Deserialize, Serialize};
+
+
+#[derive(Serialize, Deserialize)]
+pub struct ContainerEntry {
+    #[serde(alias="Id")]
+    pub id: String,
+    #[serde(alias="Names")]
+    names: Vec<String>,
+    #[serde(alias="Command")]
+    command: String,
+    #[serde(alias="Created")]
+    created: i64,
+    #[serde(alias="State")]
+    state: String,
+    #[serde(alias="Status")]
+    status: String,
+    // #[serde(alias="NetworkSettings")]
+    // network_settings: HashMap<String, HashMap<String, HashMap<String, Value>>>,
+}
+
+impl ContainerEntry {
+    pub fn to_view_vector(&self) -> [&str; 2] {
+        let name: &str = match self.names.first() {
+            Some(n) => n,
+            None => ""
+        };
+
+        [
+            name,
+            &self.id,
+        ]
+    }
+}
