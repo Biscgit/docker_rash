8f07c4ff20e743ca2fe6e6fd0cedb99cf3cd8055
David Horvát <horvatda@proton.me>
2024-03-24 18:00:46 +0100

    adjusted colour

--- src/ui.rs
+++ src/ui.rs
@@ -39,7 +39,7 @@ fn set_background(color: Color, frame: &mut Frame, area: Rect) -> Rect {
 /// renders the sidebar in the current app's state
 fn render_sidebar(app: &mut App, frame: &mut Frame, area: Rect) {
     // set background
-    set_background(Color::Rgb(32, 32, 38), frame, area);
+    set_background(Color::Rgb(34, 34, 40), frame, area);
     let mut state = ListState::default();
     state.select(Some(1));
 
@@ -88,7 +88,7 @@ fn render_sidebar(app: &mut App, frame: &mut Frame, area: Rect) {
     };
 
     // render
-    let pages = ["  Statistics", "  Containers", "  Images", "󰋊  Volumes"];
+    let pages = ["  Statistics", "  Containers", "  Images", "󰋊  Volumes"];
     pages
         .iter()
         .enumerate()
@@ -97,3 +97,99 @@ fn render_sidebar(app: &mut App, frame: &mut Frame, area: Rect) {
     // add credit at bottom
     stylize((sidebar_layout.len() - 1, &"  Credits"));
 }
+
+
+
+
+
+
+
+
+
+
+pub fn test_render(app: &mut App, frame: &mut Frame) {
+    // let area: Rect = set_background(frame);
+    let area: Rect = frame.size();
+    let example_vector = vec![
+        CTE::example(),
+        CTE::example(),
+        CTE::example(),
+    ];
+
+    let header = Row::new(["A", "B", "C"]);
+    let rows = vec![
+        Row::new(["1", "2", "3"]),
+        Row::new(["1", "2", "3"]),
+        Row::new(["1", "2", "3"]),
+    ];
+
+    let table = Table::new(
+        rows,
+        [
+            Constraint::Min(10),
+            Constraint::Min(10),
+            Constraint::Min(10),
+        ],
+    )
+        .header(header);
+
+    frame.render_widget(table, area);
+}
+
+pub struct ContainerList {
+    selected: Option<usize>,
+    elements: Vec<ContainerElement>,
+}
+
+impl Default for ContainerList {
+    fn default() -> Self {
+        ContainerList {
+            selected: None,
+            elements: Vec::with_capacity(16),
+        }
+    }
+}
+
+impl ContainerList {
+    pub fn set_index(&mut self, index: Option<usize>) {
+        match index {
+            Some(i) => {
+                let list_length = self.elements.len();
+                if list_length > 0 && i < list_length {
+                    self.selected = index;
+                }
+            }
+            None => {
+                self.selected = None;
+            }
+        }
+    }
+
+    pub fn next(&mut self) {
+        if let Some(index) = self.selected {
+            if self.elements.len() > 0 {
+                self.selected = Some((index + 1).rem_euclid(self.elements.len()));
+            }
+        }
+    }
+
+    pub fn previous(&mut self) {
+        if let Some(index) = self.selected {
+            if self.elements.len() > 0 {
+                self.selected = Some((index - 1).rem_euclid(self.elements.len()));
+            }
+        }
+    }
+}
+
+impl Widget for ContainerList {
+    fn render(self, area: Rect, buf: &mut Buffer) where Self: Sized {
+        todo!("Implement rendering");
+    }
+}
+
+pub struct ContainerElement {}
+
+pub struct ContainerEntry {}
+
+
